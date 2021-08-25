65d47b016359b139ad20bcb1175ae929c171294a
chinorlando <linusistemintearti@hotmail.com>
2021-08-25 11:54:21 -0400

    v9, validacion de camposd

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './models/register.dto';
 
 @Controller('register')
 export class AuthController {
@@ -8,7 +9,10 @@ export class AuthController {
 
 
     @Post()
-    async register(@Body() body){
+    async register(@Body() body:RegisterDto){
+        if (body.password !== body.password_confirm) {
+            throw new BadRequestException("Contraseña con coincide");
+        }
         const hashedPassword = await bcrypt.hash(body.password, 12);
         return await this.userService.create({
             first_name: body.first_name,
