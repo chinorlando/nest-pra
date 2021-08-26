c60317808b6cda80c44c8a48d32f186a61044ebc
chinorlando <linusistemintearti@hotmail.com>
2021-08-26 18:19:45 -0400

    v26 CRUD Productos

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { PermissionModule } from './permission/permission.module';
     CommonModule,
     RoleModule,
     PermissionModule,
+    ProductModule,
   ],
   controllers: [],
   providers: [],
